84f8571ad95ed4054152cc768a57473973b088bf
Jefferson Amorim <jeffersonjunior.amorim@gmail.com>
2024-05-14 11:18:29 -0400

    add exemplo de declaracao de funcao com parametros

---
+++ funcoes/declaracao-funcao-com-parametros.js
@@ -0,0 +1,7 @@
+function nomeDaFuncao(parametros1, parametros2) {
+    // corpo da funcao
+    // pode conter uma ou mais instrucoes
+    return resultado; // Opcional
+}
+// Chamado da funcao 
+nomeDaFuncao(parametros1, parametros2);
