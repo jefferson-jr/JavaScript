5a717d9c4fef4a642ee7aad760883791d5bd2103
Jefferson Amorim <jeffersonjunior.amorim@gmail.com>
2024-05-14 09:46:45 -0400

    add exemplo    parseInt.js

---
+++ parseInt.js
@@ -0,0 +1,7 @@
+let stringParaNumero = "100";
+console.log(stringParaNumero);
+console.log(typeof(stringParaNumero));
+console.log("");
+let numero = parseInt(stringParaNumero);
+console.log(numero);
+console.log(typeof(numero));
