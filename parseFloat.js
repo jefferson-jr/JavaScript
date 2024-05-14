de1004bfaf1882c16374264639e444c0101adc22
Jefferson Amorim <jeffersonjunior.amorim@gmail.com>
2024-05-14 09:46:22 -0400

    add exemplo      parseFloat.js

---
+++ parseFloat.js
@@ -0,0 +1,7 @@
+let stringParaDecimal = "12.60";
+console.log(stringParaDecimal);
+console.log(typeof(stringParaDecimal));
+console.log("");
+let decimal = parseFloat(stringParaDecimal);
+console.log(decimal);
+console.log(typeof(decimal));
