dbc3f37f9f350d16f2fe5ffc3a028e06d681d0c3
Jefferson Amorim <jeffersonjunior.amorim@gmail.com>
2024-05-14 09:44:00 -0400

    add exemplo   estrutura-de-repeticao/doWhile.js

---
+++ estrutura-de-repeticao/doWhile.js
@@ -0,0 +1,8 @@
+let numero = 1;
+let soma = 0;
+do {
+    soma += numero;
+    numero++;
+} while (numero <= 5);
+console.log("A soma dos numeros de 1 a 5 e:", soma);
+// A soma dos numeros de 1 a 5 e: 15
