d38ac6a62f4cf42ed94d222642b046e15a156a61
Jefferson Amorim <jeffersonjunior.amorim@gmail.com>
2024-05-14 09:39:59 -0400

    add exemplo   dados-primitivos.js

---
+++ dados-primitivos.js
@@ -0,0 +1,13 @@
+let nome = "Jefferson";
+let verdadeiro = true;
+let falso = false;
+let numero = 17;
+let reais = 25.99;
+let nulo = null;
+let indefinido;
+let NotANumber = 0 / 0;
+let objeto = {
+     name: "Jefferson",
+     age: 17,
+     profession: "teacher"
+}
