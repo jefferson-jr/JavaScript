25afd2cd2d1aec2fb17323fb5c85b10a26d8d32b
Jefferson Amorim <jeffersonjunior.amorim@gmail.com>
2024-05-14 09:26:16 -0400

    add exemplo de camparacao variaveis

---
+++ Operadores/comparacao-strings.js
@@ -0,0 +1,8 @@
+let stringsIguais = "Wanderson" == "Wanderson"; // true
+let stringsDiferentes = "wanderson" == "Wanderson"; // false
+let stringsMenor = "A" < "B"; // true
+let stringsMaior = "c" > "D"; // false
+console.log(`Wanderson == Wanderson: ${stringsIguais}`);
+console.log(`wanderson == Wanderson: ${stringsDiferentes}`);
+console.log(`A < B: ${stringsMenor}`);
+console.log(`C > D: ${stringMaior}`);
