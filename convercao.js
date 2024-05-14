7d99400b28b9fa1c4b0b54ba8d543d75f9b2d64a
Jefferson Amorim <jeffersonjunior.amorim@gmail.com>
2024-05-14 09:38:57 -0400

    add exemplo convercao

---
+++ convercao.js
@@ -0,0 +1,7 @@
+let concatencao = "1.1" + "1.1";
+let converterParaNumero = +"1.1";
+console.log(concatencao);
+console.log(typeof(concatencao));
+console.log("");
+console.log(converterParaNumero);
+console.log(typeof(converterParaNumero));
