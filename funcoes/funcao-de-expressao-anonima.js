8f7686a72215d946bdfbbaea52d75670f5e4ac49
Jefferson Amorim <jeffersonjunior.amorim@gmail.com>
2024-05-16 08:25:07 -0400

    funcao-de-expressao-anonima.js

---
+++ funcoes/funcao-de-expressao-anonima.js
@@ -0,0 +1,7 @@
+// funcao de expressao para gerar um numero aleatorio entre um intervalo especificado
+const gerarNumeroAleatorio = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+// Chamando a funcao para gerar um numero aleatorio entre 1 e 100
+const numeroAleatorio = gerarNumeroAleatorio(1, 100);
+console.log("Numero aleatorio:", numeroAleatorio);
